1cbb30684aa3537e42474f13bcf32a6111aed1bc
remerico@gmail.com <remerico@gmail.com@140b4ea2-ba7e-df39-444d-31ebc172e76c>
2011-02-18 06:07:51 +0000

    - Moved directories

--- trunk/src/Resources/fontlist.h
+++
@@ -1,9 +0,0 @@
-#ifndef FONT_LIST_H
-#define FONT_LIST_H
-
-#define FONT_COUNT 1
-
-#define FONTS_DEFAULT  "default.ttf"
-
-
-#endif
--- trunk/src/Resources/spritelist.h
+++
@@ -1,6 +0,0 @@
-#ifndef SPRITE_LIST_H
-#define SPRITE_LIST_H
-
-
-
-#endif
